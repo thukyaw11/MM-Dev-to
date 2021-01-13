7ed89028bf1d63affea734c478f2abc0f5acfc12
thukyaw11 <helloeleven968@gmail.com>
2021-01-13 20:54:04 +0630

    feat: title updated

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
     // Global page headers (https://go.nuxtjs.dev/config-head)
     head: {
         titleTemplate: '%s - BLOG',
-        title: 'BLOG',
+        title: 'DEV.TO MM',
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
